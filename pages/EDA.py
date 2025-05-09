b1e7ba7c66f1216e1a4642133eefe7176ec2270e
BenjaminNgJL <e0957368@u.nus.edu>
2025-05-09 10:23:15 +0800

    trial commit

---
+++ pages/EDA.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("EDA")
+st.write("This is the page for EDA.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="My Multi-Page App", layout="wide")
+
+st.title("Welcome to My Streamlit App")
+st.write("Use the sidebar to navigate between pages.")
